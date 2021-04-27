00fc1cd5d893951fbab010b7886306329dd6a5aa
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-04-27 10:49:22 -0600

    Homepage Responsive Design

--- components/Header.js
+++ components/Header.js
@@ -1,47 +1,76 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import PersonIcon from '@material-ui/icons/Person';
 import MenuIcon from '@material-ui/icons/Menu';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CloseIcon from '@material-ui/icons/Close';
+
+import styles from '../styles/Header.module.css'
+
+
 
 const Header = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);  
+
     return (
+        
         <>
         <header className="desktop-header">
-            <div className="logo_container">
-                <a>
-                    <img className="header-logo" src="https://cdn.shopify.com/s/files/1/0456/6820/4706/files/Sharebox_editable-17_180x.png?v=1605634439"></img>
-                </a>
-            </div>
-            <div className="nav_container">
-                <nav>
-                    <ul className="nav-links">
-                        <li><a href="#">Box to Go!</a></li>
-                        <li><a href="#">Make your Box</a></li>
-                        <li><a href="#">Out of the Box</a></li>
-                        <li><a href="#">Caring for you</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div className="icons_container">
-                <nav>
-                    <ul className="nav-icons">
-                        <li className="icon-item">
-                            <div className="icon">
-                                <a>
-                                    <ShoppingBasketIcon/>
-                                </a>
-                            </div>
+            <nav>
+                <ul className="menu">
+                    <li className="logo">
+                        <div className="logo_container">
+                            <a>
+                                <img className="header_logo" src="https://cdn.shopify.com/s/files/1/0456/6820/4706/files/Sharebox_editable-17_180x.png?v=1605634439"></img>
+                            </a>
+                        </div>
+                    </li>
+                    <div className= {click ? ('item nav_container_active') : ('item nav_container_innactive')}>
+                        <li className="item has_submenu">
+                            <a tabIndex="0">Box to Go!</a>
+                            <ul className="submenu">
+                                <li className="subitem"><a href="#">All boxes</a></li>
+                                <li className="subitem"><a href="#">For Him</a></li>
+                                <li className="subitem"><a href="#">For Her</a></li>
+                                <li className="subitem"><a href="#">Valentine's</a></li>
+                            </ul>
                         </li>
-                        <li className="icon-item">
-                            <div className="icon">
-                                <a>
-                                    <PersonIcon/>
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+                        <li className="item"><a href="#">Make your Box</a></li>
+                        <li className="item"><a href="#">Out of the Box</a></li>
+                        <li className="item"><a href="#">Caring for you</a></li>
+                    </div>
+                    <li className="item icon_section">
+                        <div className="icon_container">
+                            <a href="#">
+                                <ShoppingBasketIcon className="icon" style={{ fontSize: 28 }}/>
+                            </a>
+                        </div>
+                        <div className="icon_container">
+                            <a href="#">
+                                <PersonIcon className="icon" style={{ fontSize: 30 }}/>
+                            </a>
+                        </div>
+                        <div className="menu_options" onClick={handleClick}>
+                            {click ? (
+                                <div className='icon_container'>
+                                    <a href="#">
+                                        <CloseIcon className="icon" style={{ fontSize: 33 }}/>
+                                    </a>
+                                </div>
+                            ) : (
+                                <div className='icon_container'>
+                                    <a href="#">
+                                        <MenuIcon className="icon" style={{ fontSize: 33 }}/>
+                                    </a>
+                                </div> 
+                            )}
+                        </div>
+                    </li>
+                </ul>
+            </nav>
         </header>
         </>
     )
